5f9079e46e2cdcf4be6dd39e114671cf2e744bd2
kopi1996 <kopipakee@gmail.com>
2021-08-02 23:39:28 +0530

    Completed

---
+++ main.py
@@ -0,0 +1,88 @@
+import itertools
+
+DNA = ["accaccggataggctgcttattaggtccaaaaggtagtatcgtaataatggctcagccatgtctgtgcggcattccac",
+       "gaaatggttcggtgcgatatcaggccgttctcttaacttggcggtgcagatccgaacgtctggaggggtcgtgcgcta",
+       "atgtatactagacattctaacgctcgcttattggcggagaccatttgctccactacaaggctactgtgtagatccgta"
+        ,"ctacctatgtaaaacaacatctactaacgtagtccggtctttcctgatctgccctaacctacaggtcgatccgaaattcg"
+       ]
+
+
+
+t = 4
+n = 78
+l = 8
+
+
+
+def score(DNA, s):
+    diffArr = ['a', 'c', 'g', 't']
+    arr = find_matrix(DNA, s)
+
+    valueMatrix = initailize_value_matrix(diffArr)
+
+    for i in range(len(diffArr)):
+        for j in range(l):
+            for k in range(t):
+                if diffArr[i].lower() == arr[k][j].lower():
+                    valueMatrix[i][j] += 1
+
+    scoreSum = find_sum(valueMatrix)
+
+    return scoreSum
+
+
+def find_sum(valueMatrix):
+    scoreSum = 0
+    for i in range(l):
+        maxValue = 0
+        for j in range(len(valueMatrix)):
+            if valueMatrix[j][i] > maxValue:
+                maxValue = valueMatrix[j][i]
+        scoreSum += maxValue
+    return scoreSum
+
+
+def initailize_value_matrix(diffArr):
+    valueMatrix = []
+    for i1 in range(len(diffArr)):
+
+        temp = []
+        for j1 in range(l):
+            temp.append(0)
+        valueMatrix.append(temp)
+    return valueMatrix
+
+
+def find_matrix(DNA, s):
+    arr = []
+    k = 0
+    for d in DNA:
+        tempArr = []
+        for w in d[s[k]:s[k] + l]:
+            tempArr.append(w)
+
+        k += 1
+        arr.append(tempArr)
+    return arr
+
+
+def brute_force(DNA, t, n, l):
+
+    data = itertools.product(range(n - l+1), repeat=t)
+
+    bestScore = 0
+    bestMotif = []
+    for s in data:
+
+        sco = score(DNA, s)
+        if sco > bestScore:
+            bestMotif = s
+            bestScore = sco
+
+    print("Best Score: ", bestScore)
+    print("Best Motif: ", bestMotif)
+
+    return bestMotif
+
+
+brute_force(DNA, t, n, l)
